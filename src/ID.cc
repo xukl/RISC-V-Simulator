f85e252b69b8c3b863cc6a9d5d6e50c828fbee7b
xukl <xu2k3l4@outlook.com>
2020-07-13 11:21:50 +0800

    CHANGE in ID.cc.
    Abandon the use of ID_pending_result.

--- src/ID.cc
+++ src/ID.cc
@@ -23,26 +23,25 @@ constexpr inline uint32_t sign_ext_bit(int pos, uint32_t orig)
 			static_cast<int32_t>(orig & 0x80000000) >> (31 - pos));
 }
 
-static ID_inst ID_pending_result;
 #define ID_bitmask(start_pos, len)\
-	((ID_pending_result.orig >> (start_pos)) & ((1 << (len)) - 1))
+	((ID_result.orig >> (start_pos)) & ((1 << (len)) - 1))
 
 #define funct3_case(funct3_val, op)\
 				case funct3_val:\
-					ID_pending_result.exact_op = inst_op::op;\
+					ID_result.exact_op = inst_op::op;\
 					break;
 void instruction_R()
 {
-	ID_pending_result.format = inst_format::R;
-	ID_pending_result.rd = ID_bitmask(7, 5);
-	ID_pending_result.funct3 = ID_bitmask(12, 3);
-	ID_pending_result.rs1 = ID_bitmask(15, 5);
-	ID_pending_result.rs2 = ID_bitmask(20, 5);
-	ID_pending_result.funct7 = ID_bitmask(25, 7);
-	switch (ID_pending_result.funct7)
+	ID_result.format = inst_format::R;
+	ID_result.rd = ID_bitmask(7, 5);
+	ID_result.funct3 = ID_bitmask(12, 3);
+	ID_result.rs1 = ID_bitmask(15, 5);
+	ID_result.rs2 = ID_bitmask(20, 5);
+	ID_result.funct7 = ID_bitmask(25, 7);
+	switch (ID_result.funct7)
 	{
 		case 0:
-			switch (ID_pending_result.funct3)
+			switch (ID_result.funct3)
 			{
 				funct3_case(0, ADD)
 				funct3_case(1, SLL)
@@ -55,7 +54,7 @@ void instruction_R()
 			}
 			break;
 		case 0x20:
-			switch (ID_pending_result.funct3)
+			switch (ID_result.funct3)
 			{
 				funct3_case(0, SUB)
 				funct3_case(5, SRA)
@@ -69,22 +68,22 @@ void instruction_R()
 }
 void instruction_I()
 {
-	ID_pending_result.format = inst_format::I;
-	ID_pending_result.rd = ID_bitmask(7, 5);
-	ID_pending_result.funct3 = ID_bitmask(12, 3);
-	ID_pending_result.rs1 = ID_bitmask(15, 5);
-	ID_pending_result.rs2 = 0;
-	ID_pending_result.imm = sign_ext(11, ID_bitmask(20, 12));
-	switch (ID_pending_result.opcode)
+	ID_result.format = inst_format::I;
+	ID_result.rd = ID_bitmask(7, 5);
+	ID_result.funct3 = ID_bitmask(12, 3);
+	ID_result.rs1 = ID_bitmask(15, 5);
+	ID_result.rs2 = 0;
+	ID_result.imm = sign_ext(11, ID_bitmask(20, 12));
+	switch (ID_result.opcode)
 	{
 		case inst_opcode::JALR:
-			if (ID_pending_result.funct3 == 0)
-				ID_pending_result.exact_op = inst_op::JALR;
+			if (ID_result.funct3 == 0)
+				ID_result.exact_op = inst_op::JALR;
 			else
 				abort();
 			break;
 		case inst_opcode::LOAD:
-			switch (ID_pending_result.funct3)
+			switch (ID_result.funct3)
 			{
 				funct3_case(0, LB)
 				funct3_case(1, LH)
@@ -96,7 +95,7 @@ void instruction_I()
 			}
 			break;
 		default: // case inst_opcode::OP_IMM:
-			switch (ID_pending_result.funct3)
+			switch (ID_result.funct3)
 			{
 				funct3_case(0, ADDI)
 				funct3_case(2, SLTI)
@@ -105,15 +104,15 @@ void instruction_I()
 				funct3_case(6, ORI)
 				funct3_case(7, ANDI)
 				case 1:
-					if (ID_pending_result.imm >> 5 != 0)
+					if (ID_result.imm >> 5 != 0)
 						abort();
-					ID_pending_result.exact_op = inst_op::SLLI;
+					ID_result.exact_op = inst_op::SLLI;
 					break;
 				case 5:
-					if (ID_pending_result.imm >> 5 == 0)
-						ID_pending_result.exact_op = inst_op::SRLI;
-					else if (ID_pending_result.imm >> 5 == 0x20)
-						ID_pending_result.exact_op = inst_op::SRAI;
+					if (ID_result.imm >> 5 == 0)
+						ID_result.exact_op = inst_op::SRLI;
+					else if (ID_result.imm >> 5 == 0x20)
+						ID_result.exact_op = inst_op::SRAI;
 					else
 						abort();
 			}
@@ -121,13 +120,13 @@ void instruction_I()
 }
 void instruction_S()
 {
-	ID_pending_result.format = inst_format::S;
-	ID_pending_result.rd = 0;
-	ID_pending_result.funct3 = ID_bitmask(12, 3);
-	ID_pending_result.rs1 = ID_bitmask(15, 5);
-	ID_pending_result.rs2 = ID_bitmask(20, 5);
-	ID_pending_result.imm = sign_ext(11, ID_bitmask(7, 5) | (ID_bitmask(25, 7) << 5));
-	switch (ID_pending_result.funct3)
+	ID_result.format = inst_format::S;
+	ID_result.rd = 0;
+	ID_result.funct3 = ID_bitmask(12, 3);
+	ID_result.rs1 = ID_bitmask(15, 5);
+	ID_result.rs2 = ID_bitmask(20, 5);
+	ID_result.imm = sign_ext(11, ID_bitmask(7, 5) | (ID_bitmask(25, 7) << 5));
+	switch (ID_result.funct3)
 	{
 		funct3_case(0, SB)
 		funct3_case(1, SH)
@@ -138,17 +137,17 @@ void instruction_S()
 }
 void instruction_B()
 {
-	ID_pending_result.format = inst_format::B;
-	ID_pending_result.rd = 0;
-	ID_pending_result.funct3 = ID_bitmask(12, 3);
-	ID_pending_result.rs1 = ID_bitmask(15, 5);
-	ID_pending_result.rs2 = ID_bitmask(20, 5);
-	ID_pending_result.imm =
+	ID_result.format = inst_format::B;
+	ID_result.rd = 0;
+	ID_result.funct3 = ID_bitmask(12, 3);
+	ID_result.rs1 = ID_bitmask(15, 5);
+	ID_result.rs2 = ID_bitmask(20, 5);
+	ID_result.imm =
 		(ID_bitmask(7, 1) << 11) |
 		(ID_bitmask(8, 4) << 1) |
 		(ID_bitmask(25, 6) << 5) |
-		sign_ext_bit(12, ID_pending_result.orig);
-	switch (ID_pending_result.funct3)
+		sign_ext_bit(12, ID_result.orig);
+	switch (ID_result.funct3)
 	{
 		funct3_case(0, BEQ)
 		funct3_case(1, BNE)
@@ -162,32 +161,32 @@ void instruction_B()
 }
 void instruction_U()
 {
-	ID_pending_result.format = inst_format::U;
-	ID_pending_result.rd = ID_bitmask(7, 5);
-	ID_pending_result.rs1 = 0;
-	ID_pending_result.rs2 = 0;
-	ID_pending_result.imm = ID_bitmask(12, 20) << 12;
-	switch (ID_pending_result.opcode)
+	ID_result.format = inst_format::U;
+	ID_result.rd = ID_bitmask(7, 5);
+	ID_result.rs1 = 0;
+	ID_result.rs2 = 0;
+	ID_result.imm = ID_bitmask(12, 20) << 12;
+	switch (ID_result.opcode)
 	{
 		case inst_opcode::LUI:
-			ID_pending_result.exact_op = inst_op::LUI;
+			ID_result.exact_op = inst_op::LUI;
 			break;
 		default: // case inst_opcode::AUIPC:
-			ID_pending_result.exact_op = inst_op::AUIPC;
+			ID_result.exact_op = inst_op::AUIPC;
 	}
 }
 void instruction_J()
 {
-	ID_pending_result.format = inst_format::J;
-	ID_pending_result.rd = ID_bitmask(7, 5);
-	ID_pending_result.rs1 = 0;
-	ID_pending_result.rs2 = 0;
-	ID_pending_result.imm =
+	ID_result.format = inst_format::J;
+	ID_result.rd = ID_bitmask(7, 5);
+	ID_result.rs1 = 0;
+	ID_result.rs2 = 0;
+	ID_result.imm =
 		(ID_bitmask(12, 8) << 12) |
 		(ID_bitmask(20, 1) << 11) |
 		(ID_bitmask(21, 10) << 1) |
-		sign_ext_bit(20, ID_pending_result.orig);
-	ID_pending_result.exact_op = inst_op::JAL;
+		sign_ext_bit(20, ID_result.orig);
+	ID_result.exact_op = inst_op::JAL;
 }
 #undef funct3_case
 void ID()
@@ -203,52 +202,47 @@ void ID()
 	{
 		ID_stall = true;
 		ID_pause = false;
-		ID_pending_result = ID_NOP;
-		ID_pending_result.pc = pc - 4;
-		ID_result = ID_pending_result;
+		ID_result = ID_NOP;
+		ID_result.pc = pc - 4;
 		return;
 	}
 	else
 	{
-		if (!ID_pause)
+		ID_result.orig = IF_result.orig;
+		ID_result.opcode = inst_opcode(ID_bitmask(0, 7));
+		switch (ID_result.opcode)
 		{
-			ID_pending_result.orig = IF_result.orig;
-			ID_pending_result.opcode = inst_opcode(ID_bitmask(0, 7));
-			switch (ID_pending_result.opcode)
-			{
-				case inst_opcode::OP:
-					instruction_R();
-					break;
-				case inst_opcode::OP_IMM:
-				case inst_opcode::LOAD:
-				case inst_opcode::JALR:
-					instruction_I();
-					break;
-				case inst_opcode::STORE:
-					instruction_S();
-					break;
-				case inst_opcode::BRANCH:
-					instruction_B();
-					break;
-				case inst_opcode::LUI:
-				case inst_opcode::AUIPC:
-					instruction_U();
-					break;
-				case inst_opcode::JAL:
-					instruction_J();
-					break;
-				default:
-					abort();
-			}
+			case inst_opcode::OP:
+				instruction_R();
+				break;
+			case inst_opcode::OP_IMM:
+			case inst_opcode::LOAD:
+			case inst_opcode::JALR:
+				instruction_I();
+				break;
+			case inst_opcode::STORE:
+				instruction_S();
+				break;
+			case inst_opcode::BRANCH:
+				instruction_B();
+				break;
+			case inst_opcode::LUI:
+			case inst_opcode::AUIPC:
+				instruction_U();
+				break;
+			case inst_opcode::JAL:
+				instruction_J();
+				break;
+			default:
+				abort();
 		}
 		ID_pause =
-			reg_has_pending_write[ID_pending_result.rs1] ||
-			reg_has_pending_write[ID_pending_result.rs2];
+			reg_has_pending_write[ID_result.rs1] ||
+			reg_has_pending_write[ID_result.rs2];
 	}
 	if (!ID_pause)
 	{
 		ID_stall = false;
-		ID_result = ID_pending_result;
 		ID_result.rs1_val = reg[ID_result.rs1];
 		ID_result.rs2_val = reg[ID_result.rs2];
 		ID_result.pc = IF_result.pc;
